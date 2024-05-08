b6127c840faca2452fb3b266615cbc1a868bea14
Alexandra Nava <aenava@ucsc.edu>
2024-05-07 23:45:49 -0700

    method implemented

--- RungeKutta.py
+++ RungeKutta.py
@@ -1,32 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def f(A, y):
-  f = A @ y
-  return f 
 
 def RK4(t, u0, dt, f):
   '''RK method from midterm'''
-  K1 = f(t, u0)
-  K2 = f(t + (1/2) * dt, u0 + ((1/2)*dt * K1))
-  K3 = f(t + (1/2) * dt, u0 + ((1/2)*dt * K2))
-  K4 = f(t + (1) * dt, u0 + dt*K3)
+  K1 = f(A, u0)
+  K2 = f(A, u0 + ((1/2)*dt * K1))
+  K3 = f(A, u0 + ((1/2)*dt * K2))
+  K4 = f(A, u0 + dt*K3)
 
   u_next = u0 + (dt * (1/6)) * (K1 + 2*K2 + 2*K3 + K4)
   return u_next  
 
 def RK3(t, u0, dt, f): 
     '''RK method from HW1'''
-    K1 = f(t + (1/3)*dt, u0)
-    K2 = f(t + (1/3)*dt, u0 + ((1/3) * dt * K1))
-    K3 = f(t + (2/3)*dt, u0 + ((2/3) * dt * K2))
+    K1 = f(A, u0)
+    K2 = f(A, u0 + ((1/3) * dt * K1))
+    K3 = f(A, u0 + ((2/3) * dt * K2))
     u_next = u0 + (dt * (1/4)) * (K1 + 3 * K3)
     return u_next
 
-def RK_Call(u0, t0, tn, dt, RK_method, A):
+def RK_Call(u0, t0, tn, dt, RK_method, f):
     n = int((tn-t0)/dt) #number steps
 
     ts = np.arange(t0, tn, dt)#t_{k+1}
-    ts = ts[0:n]
+    ts = ts[0 : n]
     
     system_rows, system_columns = A.shape
     us = np.zeros((int(n), int(system_rows))) #solution vector
@@ -39,11 +37,24 @@ def RK_Call(u0, t0, tn, dt, RK_method, A):
       u_last = u_next
     return ts, us
 
+def f(A, y):
+    f = A @ y
+    return f 
+
 if __name__ == "__main__":
+  '''Example Call'''
   A = np.array([[-1, 3],[-3, -1]])
   y0 = np.array([-3 , 1])
   dt = 0.01
   t0 = 0 
   tn = 10
 
-  ts, us = RK_Call(y0, t0, tn, dt , RK3, A)    
+  ts, us = RK_Call(y0, t0, tn, dt , RK3, f)    
+
+  plt.title("Numerical Solution using RK Method")
+  plt.plot(ts, us)
+  plt.grid()
+  plt.xlabel("Time")
+  plt.ylabel("y(t)")
+  plt.legend()
+  plt.show()
