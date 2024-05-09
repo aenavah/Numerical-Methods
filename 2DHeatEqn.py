de16a89c09ea6728ed1102b5161d53beb838d2a7
Alexandra Nava <aenava@ucsc.edu>
2024-05-09 15:55:09 -0700

    converted intializationand calculate to functions

--- 2DHeatEqn.py
+++ 2DHeatEqn.py
@@ -6,46 +6,43 @@ from matplotlib.animation import FuncAnimation
 print("2D heat equation solver")
 
 
-def Initialize(Length, t_n, dt):
-  pass
+def Initialize(length = 40, height = 50, n = 60, dx = 1, dt = 0.001, u_top = 100, u_bottom = 0, u_left = 0, u_right = 0):
 
+    alpha = 2 #thermal diffusivity constant
 
+    #gamma = (alpha * dt) / (dx ** 2)
+    x_nodes = length # length of plate
+    y_nodes = height # height of plate
 
-alpha = 2 #thermal diffusivity constant
-delta_x = 1 
+    us_over_time = np.empty(n)
 
-delta_t = 0.001
-gamma = (alpha * delta_t) / (delta_x ** 2)
-n = 1000
-x_nodes = 50 # length of plate
-y_nodes = 50 # height of plate
+    for k in range(0, n): #for each timestep
+        us = np.empty(x_nodes, y_nodes)
+        u_0 = 0 
+        us.fill(u_0)
 
+        u_top = 100
+        u_bottom = 0 
+        u_left = 0
+        u_right = 0 
 
-# Initialize solution: number of frames, rows, columns, and fill with zeros 
-u = np.empty((n, x_nodes, y_nodes)) 
-u_initial = 0
-u.fill(u_initial)
+        us[0, :] = u_top
+        us[-1, :] = u_bottom
+        us[:, 0] = u_left
+        us[:, -1] = u_right
+        us_over_time[k] = us
 
-# Boundary conditions
-u_top = 100.0 
-u_left = 0.0
-u_bottom = 0.0
-u_right = 0.0
+def Calculate(us_over_time, method, dx):
+    n = len(us_over_time)
+    for k in range(0, n):
+        us = us_over_time[k]
+        x_nodes, y_nodes = np.shape(us)
+        for i in np.arange(1, x_nodes):
+            for j in np.arange(1, y_nodes):
+                u_next = u[i+1, j] + u[i-1, j] + u[i, j-1] - 4*u[i,j] + u[i,j]
+        us_over_time[k+1] = u_next
+    return us_over_time
 
-# Set the initial condition
-
-# Set the boundary conditions
-u[:, (x_nodes - 1):, :] = u_top
-u[:, :, :1] = u_left
-u[:, :1, 1:] = u_bottom
-u[:, :, (x_nodes - 1):] = u_right
-
-def calculate(u):
-    for k in range(0, n - 1, 1):
-        for i in range(1, x_nodes - 1, delta_x):
-            for j in range(1, y_nodes - 1, delta_x):
-                u[k + 1, i, j] = gamma * (u[k,i+1,j] + u[k,i-1,j] + u[k,i,j+1] + u[k,i,j-1] - 4*u[k,i,j]) + u[k,i,j]
-    return u
 
 def plotheatmap(u_k, k):
     # Clear the current plot figure
@@ -62,13 +59,13 @@ def plotheatmap(u_k, k):
     return plt
 
 # Do the calculation here
-u = calculate(u)
+u = Calculate(u)
 
 def animate(k):
     plotheatmap(u[k], k)
 
 anim = animation.FuncAnimation(plt.figure(), animate, interval=1, frames=n, repeat=False)
-plt.show()
+#plt.show()
 #anim.save("heat_equation_solution.gif")
 
 print("Done!")
